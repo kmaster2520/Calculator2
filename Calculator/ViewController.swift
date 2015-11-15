550b5f29419d1a7969a518aba0ba5903d2003633
Sathvik Kadaveru <Sathvik@res380d-128-61-83-219.res.gatech.edu>
2015-11-15 16:39:14 -0500

    Cleaned out unused files

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func delButtonPressed(sender: UIButton) {
-        
+        //TODO
     }
     
     @IBAction func opButtonPressed(sender: UIButton) {
@@ -30,48 +30,5 @@ class ViewController: UIViewController {
         calculations.clearQuery()
     }
     
-    /*@IBAction func addButtonPressed(sender: UIButton) {
-        setOperator("+", withFunction: +)
-    }
-    
-    @IBAction func subtractButtonPressed(sender: UIButton) {
-        setOperator("-", withFunction: -)
-    }
-    
-    @IBAction func multiplyButtonPressed(sender: UIButton) {
-        setOperator("*", withFunction: *)
-    }
-    
-    @IBAction func divideButtonPressed(sender: UIButton) {
-        setOperator("/", withFunction: /)
-    }
-    
-    func setOperator(character: String, withFunction function: (Double, Double) -> (Double)) {
-        //DefaultOperator is part of the default CalculationDelegate
-        let customOperator = DefaultOperator(forCharacter: character, withFunction: function)
-        calculations.setOperator(customOperator)
-        
-        //again, update the text on screen
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    
-    @IBAction func equalsButtonPressed(sender: UIButton) {
-        calculations.clearInputAndSave(true)
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    
-    @IBAction func clearButtonPressed(sender: UIButton) {
-        calculations.clearInputAndSave(false)
-        resultLabel.text = calculations.resultNumber.roundedString
-    }
-    
-    @IBAction func numberButtonPressed(sender: UIButton) {
-        let buttonTitle = sender.titleLabel!.text!
-        let buttonNumber = Int(buttonTitle)! //converts String to Int
-        calculations.handleInput(buttonNumber)
-        
-        //also update the text on screen
-        resultLabel.text = calculations.resultNumber.roundedString
-    }*/
 }
 
